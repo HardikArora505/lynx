e67eedb7821f265f10b450db1246dfef7e959894
HardikArora505 <hardikarora1611@gmail.com>
2021-07-20 12:35:42 +0530

    wow

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const lyricsFinder = require("lyrics-finder")
+// req.query.artist, req.query.track
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.get("/lyrics", async (req, res) => {
     const lyrics =
-      (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+      (await lyricsFinder("Alan Walker","Lily")) || "No Lyrics Found"
     res.json({ lyrics })
   })
   
